8bd676ad2cad9a158c5110f27fb55cb26d77097f
Preet Patel <pbpatel48@myseneca.ca>
2024-02-06 20:13:17 -0500

    about-animations

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,5 +1,8 @@
+const menuTop = document.querySelector(".top-items");
+
 const menuClose = document.querySelector(".phone-items");
 function toggleMenu() {
+    
     const menuItems = document.querySelector('.menu-items');
     menuItems.classList.toggle('show');
     if(menuItems.classList.contains("show")){
@@ -8,3 +11,31 @@ function toggleMenu() {
     menuClose.src="./assets/images/Menu.svg";}
 
     }
+
+function toggleTopMenu(){
+    const contactInfo = document.querySelector('.contact-info');
+
+    contactInfo.classList.toggle('show2')
+        if(contactInfo.classList.contains('show2')){
+            menuTop.src="./assets/images/Close.svg";
+           
+        }else{
+            
+            menuTop.src="./assets/images/Menu.svg";
+        }
+    }
+
+    const links = document.querySelectorAll('.navbar a');
+
+    function handleNavClick(clickedName) {
+        links.forEach(link => {
+            const href = link.getAttribute('href');
+            const targetElement = document.querySelector(href);
+            if (href === clickedName) {
+                targetElement.style.display = 'block';
+            } else {
+                targetElement.style.display = 'none';
+            }
+        });
+    }
+    
